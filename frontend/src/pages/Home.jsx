6e8ed499df0085dc25a9dda6a603f134d1ecc8ea
Hussain <shahidshaik372@gmail.com>
2024-03-25 14:42:12 +0530

    url

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get('http://localhost:5555/books')
+      .get('https://entire-books-backend.onrender.com/books/${id}')
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
