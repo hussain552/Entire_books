0e33ecf9e5dcab92af94efdd4f4de63af4a17c55
Hussain <shahidshaik372@gmail.com>
2024-03-25 20:49:57 +0530

    kol

--- backend/index.js
+++ backend/index.js
@@ -11,15 +11,15 @@ app.use(express.json());
 
 // Middleware for handling CORS POLICY
 // Option 1: Allow All Origins with Default of cors(*)
-app.use(cors(
-{
-      origin: 'https://entire-books-frontend.onrender.com',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
-}
+// app.use(cors(
+// {
+//       origin: 'https://entire-books-frontend.onrender.com',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+// }
 
+// ));
 
-));
 // Option 2: Allow Custom Origins
 // app.use(
 //   cors({
